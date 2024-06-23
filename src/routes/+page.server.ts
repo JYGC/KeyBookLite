1517128c00bbc9807ca495c346e56f299456c1b6
Junying Chen <jygchen@gmail.com>
2024-06-23 18:03:52 +1000

    correct error

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,14 @@
 import type { PageServerLoad } from "./devices/csvimport/$types";
 
 export const load: PageServerLoad = async ({ locals }) => {
-  const resultList = await locals.backendServer.collection('devices').getFullList({
-    sort: '-created',
+  const resultList = await locals.backendServices.search('devices', { 
+    page: 1,
+    perPage: 100,
+    parameters: {
+      expand: null,
+      fields: null,
+      filter: null
+    }
   });
   return {
     resultList
