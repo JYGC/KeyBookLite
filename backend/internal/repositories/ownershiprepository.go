c7d77e1f5ba9efe644b55def464602ef9b0b2bfd
Junying Chen <jygchen@gmail.com>
2024-07-14 17:36:47 +1000

    bookmark

---
+++ backend/internal/repositories/ownershiprepository.go
@@ -0,0 +1,38 @@
+package repositories
+
+import (
+	"time"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/models"
+)
+
+type IOwnershipRepository interface{}
+
+type OwnershipRepository struct {
+	app *pocketbase.PocketBase
+}
+
+func (o OwnershipRepository) AddOwnership(ownerPersonId string, propertyId string, startOfOwnership time.Time) error {
+	ownershipsCollection, findCollectionErr := o.app.Dao().FindCollectionByNameOrId("ownerships")
+	if findCollectionErr != nil {
+		return findCollectionErr
+	}
+
+	newOwnership := models.NewRecord(ownershipsCollection)
+	newOwnership.Set("owner", ownerPersonId)
+	newOwnership.Set("property", propertyId)
+	newOwnership.Set("startdate", startOfOwnership)
+
+	if saveRecordErr := o.app.Dao().SaveRecord(newOwnership); saveRecordErr != nil {
+		return saveRecordErr
+	}
+
+	return nil
+}
+
+func NewOwnershipRepository(app *pocketbase.PocketBase) IOwnershipRepository {
+	return OwnershipRepository{
+		app,
+	}
+}
--- backend/internal/services/propertyservices.go
+++ backend/internal/services/propertyservices.go
@@ -1,32 +1,42 @@
 package services
 
 import (
-	"errors"
+	"fmt"
 	"keybook/backend/internal/repositories"
 	"time"
 
 	"github.com/pocketbase/pocketbase/models"
 )
 
-type IPropertyServices interface{}
+type IPropertyServices interface {
+	AddPropertyIfNotExists(ownerUser *models.Record, propertyAddress string, startOfOwnership time.Time) (string, error)
+}
 
 type PropertyServices struct {
+	personRepository   repositories.IPersonRepository
 	propertyRepository repositories.IPropertyRepository
 }
 
-func (p PropertyServices) AddPropertyIfNotExists(owner *models.Record, propertyAddress string, startOfOwnership time.Time) (string, error) {
+func (p PropertyServices) AddPropertyIfNotExists(loggedInUser *models.Record, propertyAddress string, startOfOwnership time.Time) (string, error) {
+	loggedInPerson, _ := p.personRepository.GetPersonForUser(loggedInUser)
+	fmt.Printf("loggedInPerson: %v\n", loggedInPerson)
+
 	propertyId, getPropertyByNameErr := p.propertyRepository.GetPropertyIdByName(propertyAddress)
 	if getPropertyByNameErr != nil {
 		return "", getPropertyByNameErr
 	}
 	if propertyId != "" {
-		return propertyId, errors.New("psroperty exists")
+		return propertyId, nil
 	}
-	return p.propertyRepository.AddNewProperty(owner, propertyAddress, startOfOwnership)
+	return p.propertyRepository.AddNewProperty(loggedInUser, propertyAddress, startOfOwnership)
 }
 
-func NewPropertyServices(propertyRepository repositories.IPropertyRepository) IPropertyServices {
+func NewPropertyServices(
+	personRepository repositories.IPersonRepository,
+	propertyRepository repositories.IPropertyRepository,
+) IPropertyServices {
 	propertyServices := PropertyServices{}
+	propertyServices.personRepository = personRepository
 	propertyServices.propertyRepository = propertyRepository
 	return propertyServices
 }
